12633c0efc743395b11dc7044996e332576b6562
KKardd <wlstjd4013@gmail.com>
2023-09-10 16:16:39 +0900

    feat: add getAll method

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -4,6 +4,7 @@ import {InjectRepository} from "@nestjs/typeorm";
 import {PostEntity} from "./entities/post.entity";
 import {Repository} from "typeorm";
 import {ulid} from "ulid";
+import {SSMCardInfo} from "./SSMCardInfo";
 
 @Injectable()
 export class PostService {
@@ -12,7 +13,7 @@ export class PostService {
         private postRepository: Repository<PostEntity>
     ) {}
 
-    async save(title: string, sentence: Array<string>): Promise<any> {
+    async save(title: string, sentence: Array<string>): Promise<PostEntity> {
         const post = new PostEntity();
         post.id = ulid();
         post.title = title;
@@ -21,14 +22,11 @@ export class PostService {
         return this.postRepository.save(post);
     }
 
-    async getAll(): Promise<any> {}
-
-    // private async savePost(title: string, sentence: JSON) {
-    //     const post = new PostEntity();
-    //     post.id = ulid();
-    //     post.title = title;
-    //     post.sentence = sentence;
-    //     post.hits = 0;
-    //     return this.postRepository.save(post);
-    // }
+    async getAll(): Promise<Array<SSMCardInfo>> {
+        return this.postRepository.find({
+            order: {
+                createdAt: "DESC",
+            },
+        });
+    }
 }
