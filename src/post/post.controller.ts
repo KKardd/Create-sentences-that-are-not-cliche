0f2bb184acb9ecd4d4566e0f2f5623bafff40d9c
KKardd <wlstjd4013@gmail.com>
2023-09-10 16:03:41 +0900

    feat: add basic post method

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,7 +1,19 @@
-import {Body, Controller, Post} from "@nestjs/common";
+import {Body, Controller, Get, Post} from "@nestjs/common";
 import {PostService} from "./post.service";
+import {CreatePostDto} from "./dto/create-post.dto";
 
 @Controller("post")
 export class PostController {
     constructor(private readonly postService: PostService) {}
+
+    @Post()
+    async save(@Body() dto: CreatePostDto): Promise<void> {
+        const {title, sentence} = dto;
+        await this.postService.save(title, sentence);
+    }
+
+    @Get()
+    async getAll() {
+        return await this.postService.getAll();
+    }
 }
--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,4 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import {Injectable} from "@nestjs/common";
+import {ConfigService} from "@nestjs/config";
+import {InjectRepository} from "@nestjs/typeorm";
+import {PostEntity} from "./entities/post.entity";
+import {Repository} from "typeorm";
+import {ulid} from "ulid";
 
 @Injectable()
-export class PostService {}
+export class PostService {
+    constructor(
+        @InjectRepository(PostEntity)
+        private postRepository: Repository<PostEntity>
+    ) {}
+
+    async save(title: string, sentence: Array<string>): Promise<any> {
+        const post = new PostEntity();
+        post.id = ulid();
+        post.title = title;
+        post.sentence = sentence;
+        post.hits = 0;
+        return this.postRepository.save(post);
+    }
+
+    async getAll(): Promise<any> {}
+
+    // private async savePost(title: string, sentence: JSON) {
+    //     const post = new PostEntity();
+    //     post.id = ulid();
+    //     post.title = title;
+    //     post.sentence = sentence;
+    //     post.hits = 0;
+    //     return this.postRepository.save(post);
+    // }
+}
